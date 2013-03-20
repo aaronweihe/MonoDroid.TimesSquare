2385981612fc97d544f191b64f6cf3182419e70c
Aaron He <hwaaron@live.com>
2013-03-20 14:36:31 -0700

    Change calendar initialization range

--- MonoDroid.TimesSquare.Sample/Home.cs
+++ MonoDroid.TimesSquare.Sample/Home.cs
@@ -16,12 +16,11 @@ namespace MonoDroid.TimesSquare.Sample
 
 			SetContentView (Resource.Layout.calendar_picker);
 
-	        var nextYear = DateTime.Now.AddYears(20);
+	        var nextYear = DateTime.Now.AddYears(1);
 
 			var calendar = FindViewById<CalendarPickerView> (Resource.Id.calendar_view);
-	        long start = DateTime.Now.Millisecond;
 			calendar.Init (DateTime.Now, DateTime.Now.AddMinutes(-1), nextYear);
-	        Logr.D("Total: {0}", DateTime.Now.Millisecond - start);
+
 		    FindViewById<Button>(Resource.Id.done_button).Click += (s, o) =>
 		        {
 		            Logr.D(TAG, "Selected time in millis: " + calendar.SelectedDate);
