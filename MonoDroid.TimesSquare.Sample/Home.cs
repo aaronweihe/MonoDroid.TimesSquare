318421499dd1db8dbe57ab4ad6cce0d9b1b19036
Aaron He <hwaaron@live.com>
2013-10-14 14:14:37 -0700

    Update the sample app to feature all three modes.

--- MonoDroid.TimesSquare.Sample/Home.cs
+++ MonoDroid.TimesSquare.Sample/Home.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Android.Content.PM;
 using Android.Widget;
 using Android.App;
 using Android.OS;
+using Java.Sql;
 
 namespace MonoDroid.TimesSquare.Sample
 {
@@ -24,6 +26,68 @@ namespace MonoDroid.TimesSquare.Sample
             calendar.Init(DateTime.Now, nextYear)
                 .InMode(CalendarPickerView.SelectionMode.Single)
                 .WithSelectedDate(DateTime.Now);
+
+            var btnSingle = FindViewById<Button>(Resource.Id.button_single);
+            var btnMulti = FindViewById<Button>(Resource.Id.button_multi);
+            var btnRange = FindViewById<Button>(Resource.Id.button_range);
+            var btnDialog = FindViewById<Button>(Resource.Id.button_dialog);
+
+            btnSingle.Click += (s, o) =>
+            {
+                btnSingle.Enabled = false;
+                btnMulti.Enabled = true;
+                btnRange.Enabled = true;
+
+                calendar.Init(DateTime.Now, nextYear)
+                    .InMode(CalendarPickerView.SelectionMode.Single)
+                    .WithSelectedDate(DateTime.Now);
+            };
+
+            btnMulti.Click += (s, o) =>
+            {
+                btnSingle.Enabled = true;
+                btnMulti.Enabled = false;
+                btnRange.Enabled = true;
+
+                var dates = new List<DateTime>();
+                for (int i = 0; i < 5; i++) {
+                    dates.Add(DateTime.Now.AddDays(3*i));
+                }
+                calendar.Init(DateTime.Now, nextYear)
+                    .InMode(CalendarPickerView.SelectionMode.Multi)
+                    .WithSelectedDates(dates);
+            };
+
+            btnRange.Click += (s, o) =>
+            {
+                btnSingle.Enabled = true;
+                btnMulti.Enabled = true;
+                btnRange.Enabled = false;
+
+                var dates = new List<DateTime>() {DateTime.Now.AddDays(3), DateTime.Now.AddDays(5)};
+                calendar.Init(DateTime.Now, nextYear)
+                    .InMode(CalendarPickerView.SelectionMode.Range)
+                    .WithSelectedDates(dates);
+            };
+
+            btnDialog.Click += (s, o) =>
+            {
+                var dialogView =
+                    (CalendarPickerView) LayoutInflater.Inflate(Resource.Layout.dialog, null, false);
+                dialogView.Init(DateTime.Now, nextYear);
+                new AlertDialog.Builder(this).SetTitle("I'm a dialog!")
+                    .SetView(dialogView)
+                    .SetNeutralButton("Dismiss",
+                        (sender, args) => { }).Create().Show();
+            };
+
+            FindViewById<Button>(Resource.Id.done_button).Click += (s, o) =>
+            {
+                calendar = FindViewById<CalendarPickerView>(Resource.Id.calendar_view);
+                Logr.D(TAG, "Selected time in millis: " + calendar.SelectedDate);
+                string toast = "Selected: " + calendar.SelectedDate;
+                Toast.MakeText(this, toast, ToastLength.Short).Show();
+            };
         }
     }
 }
